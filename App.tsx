df1cbed8f5b152030475f540d23914e7b8a69bf2
User <user@Users-MacBook-Pro.local>
2023-11-17 18:58:48 +0800

    completed 3.1

--- App.tsx
+++ App.tsx
@@ -1,17 +1,144 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import fonts from './src/themes/fonts';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import colors from './src/themes/colors';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>
-        chumasasda asdasdasdads
-        <AntDesign name="stepforward" />
-      </Text>
+    <View style={styles.post}>
+      {/* header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>Nithi</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+      {/* content */}
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles.image}
+      />
+
+      {/* footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        <Text>
+          Liked by <Text style={styles.bold}>user</Text> and{' '}
+          <Text style={styles.bold}>66</Text> others
+        </Text>
+
+        {/* Post decsription */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>User </Text>
+          Description Description Description Description Description
+          Description Description Description
+        </Text>
+        {/* comments */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>User </Text>
+            Comment Comment Comment
+          </Text>
+          <AntDesign
+            name={'hearto'}
+            size={16}
+            style={styles.icon}
+            color={colors.black}
+          />
+        </View>
+        <Text>19 December 2022</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 5,
+    marginBottom: 5,
+  },
+  icon: {marginHorizontal: 5},
+  footer: {
+    padding: 10,
+  },
+  bold: {fontWeight: fonts.weight.bold},
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+  },
+});
 
 export default App;

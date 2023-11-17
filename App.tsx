447c3d12026bd6742bd7243b6f6dc007454d6e4b
User <user@Users-MacBook-Pro.local>
2023-11-17 10:02:43 +0800

    initialize project & install vector icons

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>
+        chumasasda asdasdasdads
+        <AntDesign name="stepforward" />
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;

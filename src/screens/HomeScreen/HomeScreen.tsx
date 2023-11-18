1272caedb3fb724ca90df736f08261dfdd9191bd
User <user@Users-MacBook-Pro.local>
2023-11-18 13:59:52 +0800

    completed lesson 3-4

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import {FlatList, ScrollView, StyleSheet, View} from 'react-native';
+import FeedPost from '../../components/FeedPost';
+import posts from '../../assets/data/posts.json';
+
+const HomeScreen = () => {
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({item}) => <FeedPost post={item} />}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+};
+
+export default HomeScreen;
